3dea07b6d5ca7476b894744fa6782712fd02dc31
DD <alan0960214@gmail.com>
2025-02-16 14:32:27 +0800

    visitnumber

--- mywebside_js.js
+++ mywebside_js.js
@@ -482,6 +482,26 @@ function toggleAchievements(achievement) {
     document.addEventListener('keydown', escHandler);
 }
 
+const counterUp = window.counterUp.default
+
+const callback = entries => {
+    entries.forEach(entry => {
+        const el = entry.target
+        if (entry.isIntersecting && !el.classList.contains('is-visible')) {
+            counterUp(el, {
+                duration: 2000,
+                delay: 16,
+            })
+            el.classList.add('is-visible')
+        }
+    })
+}
+
+const IO = new IntersectionObserver(callback, { threshold: 1 })
+
+const el = document.querySelector('.counter')
+IO.observe(el)
+
 // GA
 // window.dataLayer = window.dataLayer || [];
 // function gtag() { dataLayer.push(arguments); }
